25ef42dc419de5b7fa3f18904650146fa6098c67
Zepronex <callesvensson@hotmail.se>
2023-05-24 23:50:15 +0200

    Added button to go back from about screen and space between data for functionality.

--- app/src/main/java/com/example/project/AboutActivity.java
+++ app/src/main/java/com/example/project/AboutActivity.java
@@ -1,18 +1,31 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
 
     private WebView webView;
 
+    private Button backButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_activity);
+
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            finish();
+            }
+        });
 
         webView = findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
